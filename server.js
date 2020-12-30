af07bbcc039577eafaf5a0f767deef92de7ed265
Rodrigo-Morais-Mrs <72236317+Rodrigo-Morais-Mrs@users.noreply.github.com>
2020-12-30 13:12:57 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.config.PORT); // Recebe solicitações que o deixa online
+
+const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
+
+const discord = require("discord.js")
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./config.json")
+
+
+
+client.on("ready", () => {
+  console.log('Bot is ready to play songs!')
+  client.user.setActivity(`${PREFIX}help`, {type: 2});
+});
+
+
+
+client.on("warn", info => console.log(info));
+
+client.on("error", console.error)
+
+//DEFINIING
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
+client.vote = new Map();
+
+//LETS LOAD ALL FILES
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "commands", file))
+  client.commands.set(command.name, command)
+} //LOADING DONE
+
+
+//WHEN SOMEONE MESSAGE
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args)
+    //COMMAND LOGS
+    console.log(`${message.guild.name}: ${message.author.tag} Used ${client.commands.get(command).name} in #${message.channel.name}`)
+    } catch (err) { //IF IT CATCH ERROR
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+});
+
+
+
+
+//DONT DO ANYTHING WITH THIS TOKEN lol
+client.login(TOKEN)
